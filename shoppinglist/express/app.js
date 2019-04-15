a9280129d0fbbe719f65067ea9b7ace71276fa93
Nikolaj Herting Olsen <nikolaj.herting.olsen@outlook.com>
2019-04-15 11:14:38 +0200

    - Added express server with user handling

---
+++ shoppinglist/express/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');           // Log all HTTP requests to the console
+const app = express();
+const checkJwt = require('express-jwt');    // Check for access tokens automatically
+const mongoose = require('mongoose');
+
+let dbUrl = 'mongodb://localhost/shoppingList';
+
+/****** Configuration *****/
+app.use(bodyParser.json());                 // Make sure all json data is parsed
+app.use(morgan('combined'));         // Log all requests to the console
+
+const port = (process.env.PORT || 8080);
+
+/*if (!process.env.JWT_SECRET) {
+    console.error('You need to put a secret in the JWT_SECRET env variable!');
+    process.exit(1);
+}*/
+
+/****** Middleware *****/
+
+// Additional headers to avoid triggering CORS security errors in the browser
+// Read more: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+    // intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+        // respond with 200
+        console.log("Allowing OPTIONS");
+        res.sendStatus(200);
+    } else {
+        // move on
+        next();
+    }
+});
+
+// Open paths that does not need login
+let openPaths = [
+    '/api/users/authenticate',
+    '/api/users/'
+];
+// Validate the user using authentication
+app.use(
+    checkJwt({ secret: process.env.JWT_SECRET }).unless({ path : openPaths})
+);
+app.use((err, req, res) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: err.message });
+    }
+});
+
+/****** Routes ******/
+let usersRouter = require('./users_router')();
+app.use('/api/users', usersRouter);
+
+/****** Error handling ******/
+app.use(function (err, req, res) {
+    console.error(err.stack);
+    res.status(500).send({msg: 'Something broke!'})
+});
+
+/****** Listen ******/
+app.listen(port, () => console.log(`API running on port ${port}!`));
+
+mongoose.connect(dbUrl, {useNewUrlParser: true}, (err) => {
+    console.log('mongo db connection', err);
+});
---
+++ shoppinglist/express/users_router.js
@@ -0,0 +1,82 @@
+module.exports = () => {
+    let express = require('express');
+    let router = express.Router();
+
+    const jwt = require('jsonwebtoken');
+    const bcrypt = require('bcrypt');
+    const mongoose = require('mongoose');
+
+
+    let User = mongoose.model('User', {
+        username: String,
+        password: String
+    });
+
+    router.post('/', (req, res) => {
+        let username = req.body.username;
+        let password = req.body.password;
+
+        let cryptedPass = "nothing";
+
+        bcrypt.hash(password, 10, function(err, hash){
+            cryptedPass = hash;
+
+            let newUser = new User({
+                username: username,
+                password: cryptedPass
+            });
+
+            newUser.save((err) => {
+                if(err)
+                    console.error(err)
+            });
+            console.log(`Hash generated for ${username}`);
+        });
+        res.status(501).json({msg: "POST user "});
+    });
+
+    router.put('/', (req, res) => {
+        // TODO: Implement user update (change password, etc).
+        res.status(501).json({msg: "PUT update user not implemented"});
+    });
+
+    router.post('/authenticate', (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if (!username || !password) {
+            let msg = "Username or password missing!";
+            console.error(msg);
+            res.status(401).json({msg: msg});
+            return;
+        }
+
+        User.findOne({username: username}).exec(function (err, user) {
+            console.log("#### USER ####");
+            console.log(user);
+
+            if (user) {
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if (result) {
+                        const payload = {
+                            username: username,
+                            admin: false
+                        };
+                        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+                        res.json({
+                            msg: 'User authenticated successfully',
+                            token: token
+                        });
+                    }
+                    else res.status(401).json({msg: "Password mismatch!"})
+                });
+            } else {
+                res.status(404).json({msg: "User not found!"});
+            }
+        });
+        //const user = User.find((user) => user.username === username);
+    });
+
+    return router;
+};
