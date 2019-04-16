891f892b4b7cc35913a0411050a9495b89ccb057
Nikolaj Herting Olsen <nikolaj.herting.olsen@outlook.com>
2019-04-16 17:35:19 +0200

    - Added material icons and ui dependencies - Added material ui drawer side menu

--- shoppinglist/src/App.js
+++ shoppinglist/src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 import NotFound from './NotFound';
 import ShoppingList from "./ShoppingList";
 
+import Drawer from '@material-ui/core/Drawer';
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -19,16 +28,45 @@ class App extends Component {
                 title: "Boller i karry",
                 price: 50,
                 description: "Gammeldags boller i karry"
-            }]
+            }],
+            left: false
         };
     }
 
+    toggleDrawer = (side, open) => () => {
+        this.setState({
+            [side]: open,
+        });
+    };
+
     render() {
+        const sideList = (
+            <div className="list">
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider/>
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+        );
+
         return (
             <Router>
                 <div className="container">
                     <div className="header">
-                        <a href="#">
+                        <a onClick={this.toggleDrawer('left', true)} href="#">
                             <i className="material-icons">
                                 menu
                             </i>
@@ -51,6 +89,16 @@ class App extends Component {
                             <Route component={NotFound}/>
                         </Switch>
                     </div>
+                    <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            onClick={this.toggleDrawer('left', false)}
+                            onKeyDown={this.toggleDrawer('left', false)}
+                        >
+                            {sideList}
+                        </div>
+                    </Drawer>
                 </div>
             </Router>
         );
