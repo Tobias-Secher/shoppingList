fb0162cc159bde103667397000d23b29e2ddeda4
Nikolaj Herting Olsen <nikolaj.herting.olsen@outlook.com>
2019-04-29 14:19:28 +0200

    - added "find list" button on shoppinglistform

--- shoppinglist/src/ShoppingListForm.js
+++ shoppinglist/src/ShoppingListForm.js
@@ -109,6 +109,9 @@ class ShoppingListForm extends Component {
                 <button onClick={this.handleInput}
                         type="submit" id="submitItemBtn" className="btn btn-primary">CREATE
                 </button>
+                <button
+                    id="findListBtn" className="btn btn-primary btn-bot">Find list
+                </button>
             </form>
         );
     }
