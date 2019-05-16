1e5eee37f33587dc701bc3370bff304f83c7e03d
Katerine <rionegro59>
2019-05-16 11:10:35 +0200

    push notification

--- shoppinglist/src/index.js
+++ shoppinglist/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import * as serviceWorker from './serviceWorkerInit';
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+serviceWorker.register();
 
 // Register the service worker
 // if ('serviceWorker' in navigator) {
